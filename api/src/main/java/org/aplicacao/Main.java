7ed17752e5c26fbfe77cc4b74251d9e91c810a17
Murilo Barletta <60950441+barlettaMurilo@users.noreply.github.com>
2024-10-31 11:52:45 -0300

    Arquivos originários

---
+++ api/src/main/java/org/aplicacao/Main.java
@@ -0,0 +1,22 @@
+package org.aplicacao;
+
+import org.aplicacao.servico.ApiServico;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ApiServico apiServico = new ApiServico();
+
+        try {
+            System.out.println(apiServico.getEndereco("09784060"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+}
